c8dea1882f2b189a3197706a8550b0c840dd8bf2
Joven <jovenmiclat28@gmail.com>
2025-05-07 14:01:43 +0800

    resume builder - setup

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+
+class FrontEndController extends Controller
+{
+    //
+
+    public function Index(){
+        return Inertia::render('Index/Index');
+    }
+}
---
+++ resources/js/Pages/Index/Index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1 class="text-primary">Hello world</h1>
+</template>
+
+<script>
+export default {
+    name: 'Index'
+}
+</script>
+
+<style>
+
+</style>
