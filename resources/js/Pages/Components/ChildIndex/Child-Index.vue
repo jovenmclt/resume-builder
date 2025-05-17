5f269f63ee2213098889e96d255dc4c23e3a200b
Joven <jovenmiclat28@gmail.com>
2025-05-17 10:58:32 +0800

    dashboard - done

--- resources/js/Pages/Components/ChildIndex/Child-Index.vue
+++ resources/js/Pages/Components/ChildIndex/Child-Index.vue
@@ -38,6 +38,84 @@
                         </div>
                     </div>
                 </section>
+                <section id="section2">
+                    <div class="row justify-content-center">
+                        <div class="text-center">
+                            <h4 class="fw-semibold">Get started with a <br> resume template selection.</h4>
+                        </div>
+                        <div class="col-md-10 mt-4">
+                            <div class="d-flex flex-md-nowrap flex-wrap justify-content-center gap-4">
+                                <div class="text-center rounded-4" >
+                                    <img src="./assets/DashTemplate2.png" class="w-100 px-4 py-4 rounded-3" alt="" style="background-color: #F2F2F2;">
+                                </div>
+                                <div class="text-center rounded-4" >
+                                    <img src="./assets/DashTemplate1.png" class="w-100 px-4 py-4 rounded-3" alt="" style="background-color: #F2F2F2;">
+                                </div>
+                                <div class="text-center rounded-4" >
+                                    <img src="./assets/DashTemplate3.png" class="w-100 px-4 py-4 rounded-3" alt="" style="background-color: #F2F2F2;">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="section3">
+                    <div class="row justify-content-center">
+                        <div class="text-center">
+                            <h4 class="fw-semibold">Our customers say it best</h4>
+                        </div>
+                        <div class="col-md-10 mt-4">
+                            <div id="carouselExampleDark" class="carousel carousel-dark slide rounded-3" style="background-color: #F2F2F2;">
+                                <div class="carousel-indicators">
+                                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                                </div>
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active" data-bs-interval="10000" >
+                                        <div class="text-center py-4" >
+                                            <br>
+                                            <h5 class="fw-bold text-dark mb-0">John Doe</h5>
+                                            <p class="fw-normal text-dark">Web Designer, Vishay</p>
+                                            <p class="fw-light text-dark px-5">Using this resume builder was a great experience. The interface was clean and easy to navigate, which made the whole process smooth. I was able to create and download a professional resume in just a few minutes, and it helped me feel more confident when applying for jobs.</p>
+                                            <br>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item" data-bs-interval="2000">
+                                        <div class="text-center py-4" >
+                                            <br>
+                                            <h5 class="fw-bold text-dark mb-0">Artsbyannz</h5>
+                                            <p class="fw-normal text-dark">Digital Artist, Freelancer</p>
+                                            <p class="fw-light text-dark px-5">I didn’t expect building a resume could be this easy. The templates looked modern and professional, and the step-by-step process helped me focus on the right details. I’d definitely recommend this to anyone who needs a quick and quality resume.</p>
+                                            <br>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <div class="text-center py-4" >
+                                            <br>
+                                            <h5 class="fw-bold text-dark mb-0">Joven miclat</h5>
+                                            <p class="fw-normal text-dark">FullStack Dev, Tech company</p>
+                                            <p class="fw-light text-dark px-5">I’ve tried several resume builders before, but this one stood out for its clean interface and ease of use. It guided me step-by-step, which was super helpful since I wasn’t sure how to structure my resume. I especially appreciated the ability to download my resume instantly as a PDF. Definitely recommended for anyone needing a quick and polished resume!</p>
+                                            <br>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <section id="section4" class="">
+                    <div class="row py-3 d-flex align-items-center justify-content-center sec4style">
+                        <div class="col-md-7">
+                            <div class="text-center">
+                                <h2 class="fw-semibold text-white">Professional Resumes Made Easy Start Your Career Right.</h2>
+                                <br>
+                                <button class="btn btn-primary py-lg-2 mt-3 rounded-0">Create Resume</button>
+                                <p class="fw-light text-white mt-2 mb-0" style="font-size: 12px;">No registration required</p>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <Footervue />
             </main>
         </div>
     </div>
@@ -45,18 +123,28 @@
 
 <script>
 import NavigationVue from '../Navigation/Navigation.vue'
-
+import Footervue from '../Footer/Footer.vue';
 export default {
     name: 'Child-Index',
-    components: {NavigationVue}
+    components: {NavigationVue, Footervue}
 }
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=League+Spartan:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
 
+#section4{
+    padding-bottom: 10px;
+}
 section{
     padding: calc(35px + 1rem) 0;
     font-family: "Poppins", sans-serif;
+}
+
+.sec4style{
+    background-image: url('./assets/Frame 1.png');
+    background-size: cover;
+    background-position: center;
+    min-height: 60vh;
 }
 </style>
---
+++ resources/js/Pages/Components/Footer/Footer.vue
@@ -0,0 +1,39 @@
+<template>
+    <section id="footer">
+        <div class="row justify-content-center">
+            <div class="col-md-11">
+                <div class="d-flex gap-3">
+                    <h4 class="fw-light">Follow us</h4>
+                    <p class="mb-0"><i class="bi bi-facebook fs-5"></i></p>
+                    <p class="mb-0"><i class="bi bi-linkedin fs-5"></i></p>
+                    <p class="mb-0"><i class="bi bi-twitter fs-5"></i></p>
+                </div>
+                <hr>
+                <div class="d-flex flex-wrap gap-3 justify-content-between">
+                    <div class="d-flex">
+                        <h6 class="fw-bold">Resume Builder</h6>
+                        <a href="#" class="ms-4 text-decoration-none " style="font-size: 13px;">Privacy Policy</a>
+                        <a href="#" class="ms-2 text-decoration-none " style="font-size: 13px;">Terms</a>
+                    </div>
+                    <div class="text-start ">
+                        <h5 class="fw-light " style="font-size: 13px;">© Copyright 2025,Resume Builder</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name:'Footer'
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&family=League+Spartan:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+section{
+    font-family: "Poppins", sans-serif;
+}
+</style>
